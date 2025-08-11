bf27834d053ec85ebd064fd16ac40132a9a973cb
Bumblebee <bumblebeess5577@gmail.com>
2025-08-11 08:04:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -52,7 +52,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <script defer src="https://analytics.testss1.online/script.js" data-website-id="c9dcf744-c692-4e67-98d7-6a7476f65601"></script>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22hsl(336 78% 38%)%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2240%22 font-family=%22sans-serif%22 fill=%22white%22>HotX</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22hsl(37 100% 58%)%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2240%22 font-family=%22sans-serif%22 fill=%22black%22>xxxc</text></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
